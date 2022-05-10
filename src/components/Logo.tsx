57a0cf74d8d8f57ba8c7dfcfe1ccdb3ea05011d7
Guillaume Fradin <guillaumefradin@gmail.com>
2022-05-10 01:40:07 +0100

    add swipe hints

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,10 +3,12 @@ import './Logo.css';
 export const Logo = () => {
 	return (
 		<div className="logo-container">
+			<div className="swipe-indicator left">👈 Swipe Left</div>
 			<a className="logo" href="https://polygonjs.com/docs/integrations/react">
 				<img src="/images/logos.x32.png" alt="Demo of Polygonjs + React"></img>
 				<span className="link-label">Polygonjs + React Demo</span>
 			</a>
+			<div className="swipe-indicator right">Swipe Left 👉</div>
 		</div>
 	);
 };
